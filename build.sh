fbabe005179e241c058ab7151f2a317273f1f082
Malte311 <malte.luttermann@student.uni-luebeck.de>
2020-09-25 11:34:19 +0200

    Travis & Docker

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Docker build
+IMAGE_NAME='loginsystem'
+
+echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USER" --password-stdin quay.io
+docker build -t $IMAGE_NAME .
+docker images
+
+cat VERSION | while read TAG; do
+	if [[ $TAG =~ ^#.* ]] ; then 
+		echo "Skipping $TAG";
+	else 
+		echo "Tagging Image as $TAG and pushing";
+		docker tag $IMAGE_NAME "quay.io/malte311/$IMAGE_NAME:$TAG"
+      	docker push "quay.io/malte311/$IMAGE_NAME:$TAG"
+	fi
+done
+
+# PHPDoc
+curl -L https://github.com/phpDocumentor/phpDocumentor/releases/download/v3.0.0-rc/phpDocumentor.phar --output phpDocumentator.phar
+php phpDocumentator.phar --ignore src/php/Reader.php --ignore src/php/JsonReader.php -d ./src -t ./docs --template="clean"
